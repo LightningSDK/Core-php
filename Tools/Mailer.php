b6a07720014f7cc3149623b97700efcd5417dab4
DAB <dab@dab.la>
2014-08-02 08:11:07 -0500

    Added mailer error handling

--- Tools/Mailer.php
+++ Tools/Mailer.php
@@ -6,12 +6,14 @@ require_once HOME_PATH . '/Lightning/vendor/class.phpmailer.php';
 
 class Mailer {
     protected $mailer;
+    protected $fromSet = false;
 
     public function __construct() {
-        $this->mail = new \PHPMailer();
+        $this->mail = new \PHPMailer(true);
     }
 
     public function from($email, $name = null) {
+        $this->fromSet = true;
         $this->mail->AddReplyTo($email, $name);
         $this->mail->SetFrom($email, $name);
         $this->mail->AddReplyTo($email, $name);
@@ -35,6 +37,20 @@ class Mailer {
     }
 
     public function send() {
-        return $this->mail->send();
+        // Set the default from name if it wasn't set.
+        if (!$this->fromSet) {
+            $this->from(
+                Configuration::get('site.mail_from'),
+                Configuration::get('site.mail_from_name')
+            );
+        }
+
+        // Send the message.
+        try {
+            return $this->mail->send();
+        } catch (\Exception $e) {
+            Messenger::error($e->getMessage());
+            return false;
+        }
     }
 }
