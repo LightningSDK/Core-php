b680e167a380d87d2684debe608e08aff11f85e7
DAB <dab@dab.la>
2014-08-02 14:51:41 -0500

    Updated install scripts

---
+++ install-dependencies-ubuntu.sh
@@ -0,0 +1,6 @@
+sudo apt-get install ruby-full rubygems1.8
+sudo gem install sass
+sudo gem install compass
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd ../Source/sass
+foundation create
--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cp install/index.php ../
-cp install/.htaccess ../
+cp install/.htaccess-router ../.htaccess
+chmod 777 Vendor/htmlpurifier/library/HTMLPurifier/DefinitionCache/Serializer
+
+# Create the source paths
+mkdir ../Source
+cp install/.htaccess-protected ../Source/.htaccess
