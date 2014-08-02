d910addd73428b4c4934a3c189433d368bab45cb
DAB <dab@dab.la>
2014-08-02 08:11:22 -0500

    Fixed captcha param

--- Tools/ReCaptcha.php
+++ Tools/ReCaptcha.php
@@ -5,12 +5,12 @@ namespace Lightning\Tools;
 class ReCaptcha {
     public static function render() {
         require_once HOME_PATH . '/Lightning/vendor/recaptchalib.php';
-        return recaptcha_get_html(Configuration::get('captcha.public'));
+        return recaptcha_get_html(Configuration::get('recaptcha.public'));
     }
 
     public static function verify() {
         require_once HOME_PATH . '/Lightning/vendor/recaptchalib.php';
-        $resp = recaptcha_check_answer (Configuration::get('captcha.private'),
+        $resp = recaptcha_check_answer (Configuration::get('recaptcha.private'),
             $_SERVER["REMOTE_ADDR"],
             $_POST["recaptcha_challenge_field"],
             $_POST["recaptcha_response_field"]);
