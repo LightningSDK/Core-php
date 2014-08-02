ce2caa850dc79127aaf16f1b45dec61e66c70e5a
DAB <dab@dab.la>
2014-08-02 08:12:10 -0500

    Removed data from demo site and fixed metadata output

--- View/Page.php
+++ View/Page.php
@@ -11,32 +11,23 @@ use Lightning\Tools\Template;
 use Source\AA;
 
 class Page {
+    public function __construct() {
+        Messenger::loadFromQuery();
+    }
+
     public function output() {
 
-// SEND GLOBALS TO SMARTY
+        // SEND GLOBALS TO SMARTY
         $template = Template::getInstance();
         foreach (array('title', 'keywords', 'description') as $meta_data) {
-            $template->set('page_' . $meta_data, Configuration::get($meta_data));
+            $template->set('page_' . $meta_data, Configuration::get('meta_data.' . $meta_data));
         }
         $template->set('google_analytics_id', Configuration::get('google_analytics_id'));
 
-        $template->set('user', ClientUser::getInstance());
-
-        $template->set('recent_complaints',
-            Database::getInstance()->assoc("SELECT complaint.complaint_id, post_date, officer_id, officer_last, officer_first, officer_badge, agency_id, agency_name, agency_city, agency_state, agency_short, body
-						FROM complaint
-						LEFT JOIN officer USING (officer_id)
-						LEFT JOIN agency USING (agency_id)
-						LEFT JOIN response ON (response.complaint_id = complaint.complaint_id AND response.response_id = complaint.initial_complaint+1)
-						WHERE complaint.confirmed > 0  ORDER BY post_date DESC limit 20"));
-        $template->set('aa', AA::getInstance());
         $template->set('errors', Messenger::getErrors());
         $template->set('messages', Messenger::getMessages());
 
-
         $template->set('site_name', Configuration::get('site.name'));
-//$template->set('default_image', $default_image);
-//$template->set('cms',$cms);
         $template->set('blog', Blog::getInstance());
         $template->render($template);
     }
