5ea96aa269fb12dfb21bc65826904d51317b527f
DAB <dab@dab.la>
2014-08-03 16:05:36 -0500

    Updated the page controller to use the new post/get action functions

--- Pages/Page.php
+++ Pages/Page.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Contains the content HTML page controller.
+ */
 
 namespace Lightning\Pages;
 
 use Lightning\Tools\Configuration;
 use Lightning\Tools\Database;
 use Lightning\Tools\Messenger;
+use Lightning\Tools\Output;
 use Lightning\Tools\Request;
+use Lightning\Tools\Scrub;
 use Lightning\Tools\Template;
 use Lightning\Tools\ClientUser;
 use Lightning\View\Page as PageView;
@@ -18,70 +23,25 @@ class Page extends PageView {
         $content_locator = preg_replace('/\.html$/', '', $_GET['request']) ?: 'index';
 
         // Determine if the user can edit this page.
-        $editable = $user->details['type'] >= 5;
-        $template->set('editable', $editable);
+        $template->set('editable', $user->details['type'] >= 5);
 
         // Set the page template.
         $template->set('content', 'page');
 
-        if($editable){
-            $action = Request::get('action');
-
-            if ($action == 'new'){
-                $template->set('action','new');
-            } else if($action == 'save') {
-                $page_id = Request::post('page_id', 'int');
-                $url = str_ireplace(".html", "", $_POST['url']);
-                $url = str_ireplace(".htm", "", $url);
-                $url = str_replace(" ", "_", $url);
-
-                $new_values = array(
-                    'title' => Request::post('title'),
-                    'url' => $url,
-                    'keywords' => Request::post('keywords'),
-                    'description' => Request::post('description'),
-                    'site_map' => Request::post('sitemap', 'int'),
-                    'body' => Request::post('page_body', 'html', '', '', true),
-                    'last_update' => time(),
-                );
-
-                // Save the page.
-                if ($page_id != 0) {
-                    Database::getInstance()->update('pages', $new_values, array('page_id' => $page_id));
-                } else {
-                    $page_id = Database::getInstance()->insert('pages', $new_values);
-                }
-
-                if ($_POST['action'] == "save") {
-                    $output = array();
-                    $output['status'] = "OK";
-                    $output['url'] = $url;
-                    $output['page_id'] = $page_id;
-                    header('Content-type: application/json');
-                    echo json_encode($output);
-                } else {
-                    header("Location: /{$url}.html");
-                }
-                exit;
-            }
-        }
-
-
         // LOAD PAGE DETAILS
-        if($full_page = Database::getInstance()->assoc1("SELECT * FROM pages WHERE url LIKE '{$content_locator}'")){
+        if($full_page = Database::getInstance()->selectRow('pages', array('url' => array('LIKE', $content_locator)))){
             header('HTTP/1.0 200 OK');
             if($full_page['last_update'] > 0) {
                 header("Last-Modified: ".gmdate("D, d M Y H:i:s", $full_page['last_update'])." GMT");
             }
         } else {
-            $full_page = Database::getInstance()->assoc1("SELECT * FROM pages WHERE url = '404'");
+            $full_page = Database::getInstance()->selectRow('pages', array('url' => '404'));
             header('HTTP/1.0 404 NOT FOUND');
             $full_page['url'] = $_GET['page'];
             $template->set('page_blank',true);
         }
 
         // PREPARE FORM DATA CONTENTS
-
         foreach (array('title', 'keywords', 'description') as $meta_data) {
             $full_page[$meta_data] = htmlspecialchars($full_page[$meta_data], ENT_QUOTES);
             if(!empty($full_page[$meta_data])) {
@@ -89,20 +49,64 @@ class Page extends PageView {
             }
         }
 
-        if($full_page['url'] == "" && isset($_GET['page']))
+        if($full_page['url'] == "" && isset($_GET['page'])) {
             $full_page['url'] = $_GET['page'];
-        else
+        }
+        else {
             $full_page['url'] = htmlspecialchars($full_page['url'],ENT_QUOTES);
+        }
 
         // FILL IN META INFO WITH DEFAULTS
 
         $full_page['body'] = htmlspecialchars_decode($full_page['body']);
 
 
-        $template->set("full_page", $full_page);
+        $template->set('page_header', $full_page['title']);
+        $template->set('full_page', $full_page);
     }
 
-    public function post() {
+    public function getNew() {
+        // Prepare the template for a new page.
+        $template = Template::getInstance();
+        $template->set('action','new');
 
+        // Prepare the form.
+        $this->get();
+    }
+
+    public function postSave() {
+        $user = ClientUser::getInstance();
+
+        if($user->details['type'] < 5){
+            return $this->get();
+        }
+
+        $page_id = Request::post('page_id', 'int');
+        $title = Request::post('title');
+        $url = Request::post('url', 'url');
+
+        // Create an array of the new values.
+        $new_values = array(
+            'title' => $title,
+            'url' => !empty($url) ? $url : Scrub::url($title),
+            'keywords' => Request::post('keywords'),
+            'description' => Request::post('description'),
+            'site_map' => Request::post('sitemap', 'int'),
+            'body' => Request::post('page_body', 'html', '', '', true),
+            'last_update' => time(),
+        );
+
+        // Save the page.
+        if ($page_id != 0) {
+            Database::getInstance()->update('pages', $new_values, array('page_id' => $page_id));
+        } else {
+            $page_id = Database::getInstance()->insert('pages', $new_values);
+        }
+
+        $output = array();
+        $output['status'] = "OK";
+        $output['url'] = $new_values['url'];
+        $output['page_id'] = $page_id;
+        Output::json($output);
     }
 }
