e968381ed2f625358d9e42522f9f244c23d12c78
DAB <dab@dab.la>
2014-08-03 16:10:15 -0500

    JS tracks to prevent duplicate JS includes

--- View/JS.php
+++ View/JS.php
@@ -3,45 +3,60 @@
 namespace Lightning\View;
 
 class JS {
-  protected static $included_files = array();
-  protected static $inline_scripts = array();
-  protected static $startup_scripts = array();
+    protected static $included_scripts = array();
+    protected static $inline_scripts = array();
+    protected static $startup_scripts = array();
 
-  public static function add($file) {
-    self::$included_files[$file] = $file;
-  }
-
-  public static function inline($script) {
-    self::$inline_scripts[] = $script;
-  }
-
-  public static function startup($script) {
-    self::$startup_scripts[] = $script;
-  }
-
-  public static function render() {
-    $output = '';
-
-    foreach (self::$included_files as $file) {
-      $output .= '<script language="javascript" src="' . $file . '"></script>';
+    public static function add($file) {
+        self::$included_scripts[$file] = array('file' => $file, 'rendered' => false);
     }
 
-    if (!empty(self::$inline_scripts) || !empty(self::$startup_scripts)) {
-      $output .= '<script language="javascript">';
-      foreach (self::$inline_scripts as $script) {
-        $output .= $script . "\n\n";
-      }
-
-      if (!empty(self::$startup_scripts)) {
-        $output .= '$(function(){';
-        foreach (self::$startup_scripts as $script) {
-          $output .= $script;
+    public static function inline($script) {
+        $hash = md5($script);
+        if (empty(self::$inline_scripts[$hash])) {
+            self::$inline_scripts[$hash] = array('script' => $script, 'rendered' => false);
         }
-        $output .= '})';
-      }
-      $output .= '</script>';
     }
 
-    return $output;
-  }
+    public static function startup($script) {
+        $hash = md5($script);
+        if (empty(self::$startup_scripts[$hash])) {
+            self::$startup_scripts[$hash] = array('script' => $script, 'rendered' => false);
+        }
+    }
+
+    public static function render() {
+        $output = '';
+
+        foreach (self::$included_scripts as &$file) {
+            if (empty($file['rendered'])) {
+                $output .= '<script language="javascript" src="' . $file['file'] . '"></script>';
+                $file['rendered'] = true;
+            }
+        }
+
+        if (!empty(self::$inline_scripts) || !empty(self::$startup_scripts)) {
+            $output .= '<script language="javascript">';
+            foreach (self::$inline_scripts as $script) {
+                if (empty($script['rendered'])) {
+                    $output .= $script['script'] . "\n\n";
+                    $script['rendered'] = true;
+                }
+            }
+
+            if (!empty(self::$startup_scripts)) {
+                $output .= '$(function(){';
+                foreach (self::$startup_scripts as &$script) {
+                    if (empty($script['rendered'])) {
+                        $output .= $script['script'];
+                        $script['rendered'] = true;
+                    }
+                }
+                $output .= '})';
+            }
+            $output .= '</script>';
+        }
+
+        return $output;
+    }
 }
