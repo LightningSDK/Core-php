325e4129a69fbf39bc71194e2da58cf2a94576b0
DAB <dab@dab.la>
2014-08-04 20:13:02 -0500

    Added comments to JS.php

--- View/JS.php
+++ View/JS.php
@@ -1,16 +1,53 @@
 <?
+/**
+ * @file
+ * Includes a class for managing JS files.
+ */
 
 namespace Lightning\View;
 
+/**
+ * Class JS
+ * @package Lightning\View
+ */
 class JS {
+    /**
+     * @var array
+     *
+     * An array of script files to include.
+     */
     protected static $included_scripts = array();
+
+    /**
+     * @var array
+     *
+     * A list of scripts to add into a script tag.
+     */
     protected static $inline_scripts = array();
+
+    /**
+     * @var array
+     *
+     * A list of scripts to run when the page is ready.
+     */
     protected static $startup_scripts = array();
 
+    /**
+     * Add a JS file to be included in the HTML.
+     *
+     * @param $file
+     *   The relative path to the file from the current URL request.
+     */
     public static function add($file) {
         self::$included_scripts[$file] = array('file' => $file, 'rendered' => false);
     }
 
+    /**
+     * Add an inline script to run as the page loads or to set variables.
+     *
+     * @param $script
+     *   The javascript code.
+     */
     public static function inline($script) {
         $hash = md5($script);
         if (empty(self::$inline_scripts[$hash])) {
@@ -18,6 +55,12 @@ class JS {
         }
     }
 
+    /**
+     * Add an inline script to run when the page is ready.
+     *
+     * @param string $script
+     *   The javascript code.
+     */
     public static function startup($script) {
         $hash = md5($script);
         if (empty(self::$startup_scripts[$hash])) {
@@ -25,9 +68,16 @@ class JS {
         }
     }
 
+    /**
+     * Output all the JS functions including inline, startup and resource files.
+     *
+     * @return string
+     *   The rendered output.
+     */
     public static function render() {
         $output = '';
 
+        // Include JS files.
         foreach (self::$included_scripts as &$file) {
             if (empty($file['rendered'])) {
                 $output .= '<script language="javascript" src="' . $file['file'] . '"></script>';
@@ -37,6 +87,7 @@ class JS {
 
         if (!empty(self::$inline_scripts) || !empty(self::$startup_scripts)) {
             $output .= '<script language="javascript">';
+            // Include inline scripts.
             foreach (self::$inline_scripts as $script) {
                 if (empty($script['rendered'])) {
                     $output .= $script['script'] . "\n\n";
@@ -44,6 +95,7 @@ class JS {
                 }
             }
 
+            // Include ready scripts.
             if (!empty(self::$startup_scripts)) {
                 $output .= '$(function(){';
                 foreach (self::$startup_scripts as &$script) {
