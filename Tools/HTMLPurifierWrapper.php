2b5972d1e2f72be329c8047c39594a88f3c73f60
DAB <dab@dab.la>
2014-08-05 03:09:02 -0500

    Updated static classes in HTMLpurifier wrapper

--- Tools/HTMLPurifierWrapper.php
+++ Tools/HTMLPurifierWrapper.php
@@ -5,14 +5,26 @@ namespace Lightning\Tools;
 use HTMLPurifier_Config;
 
 class HTMLPurifierWrapper extends Singleton {
-    public function createInstance() {
+    /**
+     * Get an HTMLPurifier instance.
+     *
+     * @return \HTMLPurifier
+     *   The instance.
+     */
+    public static function createInstance() {
         require_once HOME_PATH . '/Lightning/Vendor/htmlpurifier/library/HTMLPurifier.auto.php';
         return new \HTMLPurifier();
     }
 }
 
 class HTMLPurifierConfig extends Singleton {
-    public function createDefault() {
+    /**
+     * Get a HTMLPurifier_Config instance.
+     *
+     * @return \Default|HTMLPurifier_Config
+     *   The instance.
+     */
+    public static function createDefault() {
         require_once HOME_PATH . '/Lightning/Vendor/htmlpurifier/library/HTMLPurifier/Config.php';
         return \HTMLPurifier_Config::createDefault();
     }
