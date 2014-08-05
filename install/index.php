f9f6ea9b8f6d5a069a552389613bf978c4f4aeee
DAB <dab@dab.la>
2014-08-05 02:51:53 -0500

    updated index.php to require and simplified page execution

--- install/index.php
+++ install/index.php
@@ -4,7 +4,7 @@ use Lightning\Tools\Messenger;
 use Lightning\Tools\Request;
 use Lightning\Tools\Router;
 
-include 'Lightning/bootstrap.php';
+require 'Lightning/bootstrap.php';
 
 $handler = Router::getInstance()->getRoute($_GET['request']);
 
@@ -16,15 +16,7 @@ if (empty($handler)) {
 
 try {
     $page = new $handler();
-    $request_type = strtolower(Request::type());
-    if (in_array(strtolower($request_type), get_class_methods($page))) {
-        $page->$request_type();
-        $page->output();
-    } else {
-        // TODO: show 302
-        echo 'Method not available';
-        exit;
-    }
+    $page->execute();
 } catch (Exception $e) {
     $errors = Messenger::getErrors();
     array_unshift($errors, $e->getMessage());
