026bc6212ca380ef1b788539c3c8bfe87058983a
DAB <dab@dab.la>
2014-08-06 04:05:01 -0500

    changed get_class_methods() to method_exists()

--- View/Page.php
+++ View/Page.php
@@ -51,7 +51,7 @@ class Page {
         $request_type = strtolower(Request::type());
 
         if ($action) {
-            if (in_array($request_type . $action, get_class_methods($this))) {
+            if (method_exists($this, $request_type . $action)) {
                 $this->{$request_type . $action}();
                 $this->output();
             }
@@ -59,7 +59,7 @@ class Page {
                 Messenger::error('There was an error processing your submission.');
             }
         } else {
-            if (in_array($request_type, get_class_methods($this))) {
+            if (method_exists($this, $request_type)) {
                 $this->$request_type();
                 $this->output();
             } else {
