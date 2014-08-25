ac5c8fa9c34a14632409b143db5fa3af42fdacc1
DAB <dab@dab.la>
2014-08-24 19:28:42 -0500

    added sitemap

---
+++ Pages/Sitemap.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Lightning\Pages;
+
+use Lightning\Tools\Configuration;
+use Lightning\Tools\Database;
+use Lightning\Tools\Output;
+use Lightning\View\Page;
+
+class Sitemap extends Page {
+    public function get() {
+        print '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+
+        $db = Database::getInstance();
+        $web_root = Configuration::get('web_root');
+        $today = date("Y-m-d");
+        $urls = array();
+
+        // Load the pages.
+        $pages = $db->select('pages', array('site_map' => 1));
+
+        foreach($pages as $p){
+            if($p['last_update'] == 0) {
+                $p['last_update'] = time();
+            }
+            switch($p['frequency']) {
+                case 0: $fr="daily"; break;
+                case 1: $fr="weekly"; break;
+                case 2: $fr="monthly"; break;
+                case 3: $fr="annually"; break;
+            }
+
+            $urls[] = array(
+                'loc' => $web_root . "/{$p['url']}.html",
+                'lastmod' => date('Y-m-d', $p['last_update']),
+                'changefreq' => $fr,
+                'priority' => $p['priority'] / 100,
+            );
+        }
+
+        $urls[] = array(
+            'loc' => $web_root . '/directory',
+            'lastmod' => $today,
+            'changefreq' => 'weekly'
+        );
+
+        $blogs = $db->assoc('
+        SELECT blog.time AS blog_time, blog_comment.time AS blog_comment_time, url
+        FROM blog
+        LEFT JOIN ( SELECT time, blog_id FROM blog_comment ORDER BY TIME DESC) AS blog_comment USING ( blog_id )
+        GROUP BY blog_id');
+
+        foreach($blogs as $b){
+            $urls[] = array(
+                'loc' => $web_root . "/{$b['url']}.htm",
+                'lastmod' => date("Y-m-d", max($b['blog_time'],$b['blog_comment_time']) ?: time()),
+                'changefreq' => 'yearly',
+                'priority' => .3,
+            );
+        }
+
+        print Output::XMLSegment($urls, 'url');
+        print '</urlset>';
+        exit;
+    }
+}
