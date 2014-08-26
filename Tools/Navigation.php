1c03c9c684288d2d4f21e37862040fdb064f62a8
DAB <dab@dab.la>
2014-08-26 11:01:15 -0500

    added cookies before redirect

--- Tools/Navigation.php
+++ Tools/Navigation.php
@@ -4,6 +4,7 @@ namespace Lightning\Tools;
 
 class Navigation {
     public static function redirect($url) {
+        Output::sendCookies();
         header('Location: ' . $url);
         exit;
     }
