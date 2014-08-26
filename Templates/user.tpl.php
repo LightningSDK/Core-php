12da6eeff0df5af8d12e5dda3c8b1f4ff7c0183d
DAB <dab@dab.la>
2014-08-26 11:04:55 -0500

    fixed errors in user template

--- Templates/user.tpl.php
+++ Templates/user.tpl.php
@@ -1,68 +1,64 @@
 <script language="javascript">
 
-$(document).ready(function(){
-	$("#register").validate({
-        rules: {
-            password: 
-            {
-              required: true
-            },
-            password2: {
-                equalTo: "#password"
+    $(document).ready(function(){
+        $("#register").validate({
+            rules: {
+                password:
+                {
+                    required: true
+                },
+                password2: {
+                    equalTo: "#password"
+                }
             }
-        }
+        });
     });
-});
 
 </script>
 <? if (empty($l_page) || $l_page == "join" || $l_page == "register"): ?>
-  <h2>Create a username and password</h2>
-  <form action="user.php" method="post" id="register">
+    <h2>Create a username and password</h2>
+    <form action="/user" method="post" id="register">
+        <table>
+            <tr><td>Email:</td><td><input type='text' name='email' class="required email" /></td></tr>
+            <tr><td>Password:</td><td><input type='password' name='password' id='password' class="required" /></td></tr>
+            <tr><td>Confirm Password:</td><td><input type='password' name='password2' class="required" /></td></tr>
+        </table>
+        <input type="hidden" name="action" value="register" />
+        <input type="hidden" name="redirect" value="<?=!empty($redirect) ? $redirect : '';?>" />
+        <input type="submit" name="submit" value="Register" />
+    </form>
 
-  <table>
-  <tr><td>Email:</td><td><input type='text' name='email' class="required email" /></td></tr>
-  <tr><td>Password:</td><td><input type='password' name='password' id='password' class="required" /></td></tr>
-  <tr><td>Confirm Password:</td><td><input type='password' name='password2' class="required" /></td></tr>
-  </table>
-  <input type="hidden" name="action" value="register" />
-  <input type="hidden" name="redirect" value="<?=$redirect?>" />
-  <input type="submit" name="submit" value="Register" />
-
-  </form>
-
-  <br /><br />
+    <br /><br />
 <? endif; ?>
 
 <? if (empty($l_page) || $l_page == "login"): ?>
 
-  <h2>Log In with your email and password.</h2>
+    <h2>Log In with your email and password.</h2>
 
-  <form action="user.php" method="post" id="register">
+    <form action="/user" method="post" id="register">
 
-  <table>
-  <tr><td>Email:</td><td><input type='text' name='email' /></td></tr>
-  <tr><td>Password:</td><td><input type='password' name='password' /></td></tr>
-  </table>
-  <input type="hidden" name="action" value="login" />
-  <input type="hidden" name="redirect" value="<?=$redirect?>" />
-  <input type="submit" name="submit" value="Log In" />
-  <a href='user.php?p=reset'>Forgot your password?</a>
-  </form>
+        <table>
+            <tr><td>Email:</td><td><input type='text' name='email' /></td></tr>
+            <tr><td>Password:</td><td><input type='password' name='password' /></td></tr>
+        </table>
+        <input type="hidden" name="action" value="login" />
+        <input type="hidden" name="redirect" value="<?=!empty($redirect) ? $redirect : '';?>" />
+        <input type="submit" name="submit" value="Log In" />
+        <a href='/user?action=reset'>Forgot your password?</a>
+    </form>
 
 
-  <br /><br />
+    <br /><br />
 <? endif; ?>
 <? if (!empty($l_page) && $l_page == "reset"): ?>
-  <h2>Forgot your password?</h2>
+    <h2>Forgot your password?</h2>
 
-  <form action="user.php" method="post">
-
-  <table>
-  <tr><td>Enter your email address:</td><td><input type='text' name='email' /></td></tr>
-  </table>
-  <input type="hidden" name="action" value="reset" />
-  <input type="hidden" name="redirect" value="<?=$redirect?>" />
-  <input type="submit" name="submit" value="Log In" />
-
-  </form>
+    <form action="/user" method="post">
+        <table>
+            <tr><td>Enter your email address:</td><td><input type='text' name='email' /></td></tr>
+        </table>
+        <input type="hidden" name="action" value="reset" />
+        <input type="hidden" name="redirect" value="<?=!empty($redirect) ? $redirect : '';?>" />
+        <input type="submit" name="submit" value="Log In" />
+    </form>
 <? endif; ?>
