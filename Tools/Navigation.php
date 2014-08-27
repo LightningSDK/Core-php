1199b1455e1aadb148d7502c5d042c9fafa16b04
DAB <dab@dab.la>
2014-08-27 12:12:09 -0500

    changed Navigation::redirect() to take empty argument for post/get redirects

--- Tools/Navigation.php
+++ Tools/Navigation.php
@@ -3,7 +3,10 @@
 namespace Lightning\Tools;
 
 class Navigation {
-    public static function redirect($url) {
+    public static function redirect($url = null) {
+        if (empty($url)) {
+            $url = $_SERVER['REQUEST_URI'];
+        }
         Output::sendCookies();
         header('Location: ' . $url);
         exit;
