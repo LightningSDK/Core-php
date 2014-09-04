54fe1d318fda4eb16471eb227ba6e4a78f04acc9
DAB <dab@dab.la>
2014-09-03 22:31:35 -0500

    added test CLI file to installer

---
+++ install/CLI/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains Source\CLI\Test
+ */
+
+namespace Source\CLI;
+
+/**
+ * A command line handler for test execution.
+ *
+ * @package Source\CLI
+ */
+class Test {
+    public function execute() {
+        // Put your test code here.
+    }
+}
