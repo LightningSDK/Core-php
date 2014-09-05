739aa2612492eefb31f3ab9219d7a250a6ce93e2
DAB <dab@dab.la>
2014-09-04 23:22:15 -0500

    added the admin pages list page

---
+++ Pages/Admin/Pages.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Lightning\Pages\Admin;
+
+use Lightning\Pages\Table;
+
+class Pages extends Table {
+    protected $table = 'page';
+    protected $sortable = true;
+    protected $preset = array(
+        'page_id' => array(
+            'type' => 'hidden',
+        ),
+        'keywords' => array(
+            'unlisted' => true,
+            'type' => 'textarea',
+        ),
+        'description' => array(
+            'unlisted' => true,
+            'type' => 'textarea',
+        ),
+        'site_map' => array(
+            'type' => 'checkbox',
+        ),
+        'last_update' => array(
+            'type' => 'datetime',
+        )
+    );
+}
