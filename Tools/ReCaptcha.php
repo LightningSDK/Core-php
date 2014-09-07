7549ddfac6def07eb5d24e12d475a33506d84649
DAB <dab@dab.la>
2014-09-07 08:51:42 -0500

    added docblocks to recaptcha class

--- Tools/ReCaptcha.php
+++ Tools/ReCaptcha.php
@@ -1,13 +1,34 @@
 <?
+/**
+ * @file
+ * Contains Lightning\Tools\Recaptcha
+ */
 
 namespace Lightning\Tools;
 
+/**
+ * A class for rendering the Recaptcha verification elements.
+ *
+ * @package Lightning\Tools
+ */
 class ReCaptcha {
+    /**
+     * Render a ReCaptcha input.
+     *
+     * @return string
+     *   Rendered HTML.
+     */
     public static function render() {
         require_once HOME_PATH . '/Lightning/Vendor/recaptcha/recaptcha-plugins/php/recaptchalib.php';
         return recaptcha_get_html(Configuration::get('recaptcha.public'));
     }
 
+    /**
+     * Validate the CAPTCHA input.
+     *
+     * @return boolean
+     *   Whether the captcha was verified.
+     */
     public static function verify() {
         require_once HOME_PATH . '/Lightning/Vendor/recaptcha/recaptcha-plugins/php/recaptchalib.php';
         $resp = recaptcha_check_answer (Configuration::get('recaptcha.private'),
