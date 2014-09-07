99daf236b9bf9c2a367f7ee4df75abeafeda4882
DAB <dab@dab.la>
2014-09-07 08:52:09 -0500

    added field renderer for default value from post

--- View/Field.php
+++ View/Field.php
@@ -2,8 +2,29 @@
 
 namespace Lightning\View;
 
+use Lightning\Tools\Request;
+use Lightning\Tools\Scrub;
+
 class Field {
     public function render ($field) {
         return '';
+    }
+
+    /**
+     * Get the encoded default value for a form element.
+     *
+     * @param string $var
+     *   The name of the field.
+     * @param string $alt_default
+     *   A default if nothing was submitted.
+     * @param string $type
+     *   The type, usually html ot text.
+     *
+     * @return string
+     *   The HTML encoded value.
+     */
+    public static function defaultValue($var, $alt_default = null, $type = 'text') {
+        $default = Request::get($var, $type) !== null ? Request::get($var, $type) : $alt_default;
+        return Scrub::toHTML($default);
     }
 }
