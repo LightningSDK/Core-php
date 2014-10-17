61dc3cfdaa54e38ee3ab907a9ce3735fee4a6d39
DAB <dab@dab.la>
2014-10-17 13:00:33 -0500

    added query string option to Navigation::redirect()

--- Tools/Navigation.php
+++ Tools/Navigation.php
@@ -3,9 +3,12 @@
 namespace Lightning\Tools;
 
 class Navigation {
-    public static function redirect($url = null) {
+    public static function redirect($url = null, $query = array()) {
         if (empty($url)) {
             $url = $_SERVER['REQUEST_URI'];
+        }
+        if (!empty($query)) {
+            $url .= '?' . http_build_query($query);
         }
         Output::sendCookies();
         header('Location: ' . $url);
