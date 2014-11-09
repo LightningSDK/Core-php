057b6af31993da56d91e3527b1eda51f685382a0
DAB <dab@dab.la>
2014-11-08 20:20:16 -0600

    added message template table to schema

---
+++ Database/Schema/MessageTemplate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lightning\Database\Schema;
+
+use Lightning\Database\Schema;
+
+class MessageTemplate extends Schema {
+
+    protected $table = 'message_template';
+
+    public function getColumns() {
+        return array(
+            'template_id' => $this->autoincrement(),
+            'title' => $this->varchar(32),
+            'subject' => $this->varchar(64),
+            'body' => $this->text(Schema::MEDIUMTEXT),
+        );
+    }
+
+    public function getKeys() {
+        return array(
+            'primary' => 'template_id',
+        );
+    }
+}
