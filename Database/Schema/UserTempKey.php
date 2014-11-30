5637909fa540ac9ea0a4a74b4b2e25ef8b841ddb
DAB <dab@dab.la>
2014-11-29 19:34:38 -0600

    added user_temp_key to schema for recovering passwords

---
+++ Database/Schema/UserTempKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lightning\Database\Schema;
+
+use Lightning\Database\Schema;
+
+class UserTempKey extends Schema {
+
+    protected $table = 'user_temp_key';
+
+    public function getColumns() {
+        return array(
+            'user_id' => $this->int(true),
+            'temp_key' => $this->char(44),
+            'time' => $this->int(true),
+        );
+    }
+
+    public function getKeys() {
+        return array(
+            'primary' => 'user_id',
+        );
+    }
+}
