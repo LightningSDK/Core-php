7d71e2d5dfd815b89c6ecc2996721644ad83a7fd
DAB <dab@dab.la>
2014-12-04 18:05:34 -0600

    added user registration to user api

--- API/User.php
+++ API/User.php
@@ -2,7 +2,10 @@
 
 namespace Lightning\API;
 
+use Lightning\Tools\ClientUser;
+use Lightning\Tools\Configuration;
 use Lightning\Tools\Messenger;
+use Lightning\Tools\Navigation;
 use Lightning\Tools\Output;
 use Lightning\Tools\Request;
 use Lightning\Tools\Session;
@@ -22,5 +25,27 @@ class User extends API {
             $data['cookies'] = array('session' => Session::getInstance()->key);
         }
         Output::json($data);
+    }
+
+    public function postRegister() {
+        $email = Request::post('email', 'email');
+        $pass = Request::post('password');
+        $pass2 = Request::post('password2');
+        if ($email && $pass == $pass2){
+            $user = ClientUser::getInstance();
+            $previous_user = $user->id;
+            if($user_id = UserObj::create($email, $pass)){
+                UserObj::login($email, $pass2);
+                $user = ClientUser::getInstance();
+
+                if($previous_user != 0) {
+                    // TODO: This should only happen if the user is a placeholder.
+                    $user->merge_users($previous_user);
+                }
+                Output::json(array('user_id' => ClientUser::getInstance()->id));
+            }
+            Output::jsonError('User could not be created');
+        }
+        Output::jsonError('Missing data');
     }
 }
