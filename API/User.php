ee0936fac4017f16646f0ad65d0d8bd266721a4c
Alexandr Baklanov <alexandr.baklanov@gmail.com>
2014-12-13 13:43:22 +0300

    POST data validation in user registraion API

--- API/User.php
+++ API/User.php
@@ -32,15 +32,48 @@ class User extends API {
     public function postRegister() {
         $email = Request::post('email', 'email');
         $pass = Request::post('password');
-        if ($email && $pass) {
-
-            $res = UserModel::register($email, $pass);
-            if ($res['success']) {
-                Output::json($res['data']);
-            } else {
-                Output::jsonError('User could not be created');
-            }
+        
+        // Validate POST data
+        if (!$this->validateData($email, $pass)) {
+            // Immediately output all the errors
+            Output::jsonError('');
         }
-        Output::jsonError('Missing data');
+        
+        // Register user
+        $res = UserModel::register($email, $pass);
+        if ($res['success']) {
+            Output::json($res['data']);
+        } else {
+            Output::jsonError('User could not be created');
+        }
+    }
+    
+    /**
+     * Validates POST data (email, password and confirmation).
+     * 
+     * @param string $email
+     * @param string $pass
+     */
+    protected function validateData($email, $pass) {
+        // Default value
+        $result = TRUE;
+        
+        // Are all fields filled?
+        if (is_null($email) OR is_null($pass)) {
+            Messenger::error('Please fill out all the fields');
+            $result = FALSE;
+        }
+        
+        // Is email correct?
+        if ($email === FALSE) {
+            Messenger::error('Please enter a correct email');
+            $result = FALSE;
+        }
+
+        // Are passwords strong enough? Check its length
+        if (strlen($pass) < 6 OR strlen($pass2) < 6) {
+            Messenger::error('Passwords must be at least 6 characters');
+            $result = FALSE;
+        }
     }
 }
