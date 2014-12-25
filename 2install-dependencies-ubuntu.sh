a5446b4c2b19a3dde4c8832251849fed32659576
DAB <dab@dab.la>
2014-12-25 15:07:01 -0600

    updated installation for debian

--- 2install-dependencies-ubuntu.sh
+++ 2install-dependencies-ubuntu.sh
@@ -22,12 +22,16 @@ if [[ -f /etc/redhat-release ]]; then
   gem install bundle
 elif [[ "$PLATFORM" == 'Linux' ]]; then
   # add repo for nodejs
-  sudo apt-get install python-software-properties
-  sudo add-apt-repository -y ppa:chris-lea/node.js
-  sudo apt-get update
+  sudo apt-get install python-software-properties curl
+  curl -sL https://deb.nodesource.com/setup | sudo bash -
 
   # install ruby for compass and grunt
-  sudo apt-get -y install ruby-full rubygems python-software-properties python g++ make nodejs ruby-bundler
+  sudo apt-get -y install ruby-full rubygems python-software-properties python g++ make nodejs
+  if [[ -f /etc/debian_version ]]; then
+    sudo apt-get -y install bundler
+  else
+    sudo apt-get -y install ruby-bundler
+  fi
 elif [[ "$PLATFORM" == 'Darwin' ]]; then
   # install ruby for mac
   \curl -L https://get.rvm.io | bash -s stable
@@ -38,23 +42,22 @@ sudo gem install sass
 sudo gem install compass
 sudo gem install foundation
 
-  # Some git repos will rime out on git://
-  git config --global url.https://.insteadOf git://
+# Some git repos will rime out on git://
+git config --global url.https://.insteadOf git://
 
-  # Install foundation dependencies with npm and bower.
-  cd ${DIR}/Vendor/foundation
-  sudo npm install -g bower grunt-cli
+# Install foundation dependencies with npm and bower.
+cd ${DIR}/Vendor/foundation
+sudo npm install -g bower grunt-cli
 
-  # for centos, install with bundler
-  if [[ -f /etc/redhat-release ]]; then
-    sudo bundle install
-  fi
+# for centos, install with bundler
+if [[ -f /etc/redhat-release || -f /etc/debian_version ]]; then
+  sudo bundle install
+fi
 
-  # install other dependences
-  npm install
-  bower install --allow-root
-  # If github times out, try: git config --global url."https://".insteadOf git://
-  grunt build
+# install other dependences
+npm install
+bower install --allow-root
+grunt build
 
 # Install jquery Validation
 cd ${DIR}/Vendor/jquery-validation
