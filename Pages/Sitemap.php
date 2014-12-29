6ce50f94929922c67030f3a3ccab07d77d69cd55
DAB <dab@dab.la>
2014-12-29 14:50:01 -0600

    changed sitemap from Page to API

--- Pages/Sitemap.php
+++ Pages/Sitemap.php
@@ -5,9 +5,9 @@ namespace Lightning\Pages;
 use Lightning\Tools\Configuration;
 use Lightning\Tools\Database;
 use Lightning\Tools\Output;
-use Lightning\View\Page;
+use Lightning\View\API;
 
-class Sitemap extends Page {
+class Sitemap extends API {
     public function get() {
         print '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
