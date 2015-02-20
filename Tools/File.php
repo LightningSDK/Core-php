a2f51f751de9a02227f4129664c57f3f2c449a13
DAB <dab@dab.la>
2015-02-20 11:21:14 -0600

    added file tools

---
+++ Tools/File.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Lightning\Tools;
+
+class File {
+    public static function recursivePath($base_path, $file_id,$extension) {
+        $directory = str_pad($file_id,16,"0", STR_PAD_LEFT);
+        $file = substr($directory,12,4);
+        $directory = $base_path.substr($directory,0,4)."/".substr($directory,4,4)."/".substr($directory,8,4);
+        if (!is_dir($directory))
+            mkdir($directory, 0777, true);
+        return $directory."/".$file.".".$extension;
+    }
+}
