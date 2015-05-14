a037437b427faa75c94a2880122ca5316df255e9
DAB <dab@dab.la>
2015-05-13 21:19:38 -0500

    added comments to video class

--- View/Video.php
+++ View/Video.php
@@ -3,7 +3,9 @@
 namespace Lightning\View;
 
 class Video {
-
+    /**
+     * Initialize requires CSS and JS files.
+     */
     public static function initDisplay() {
         static $inited = false;
         if (!$inited) {
@@ -14,6 +16,15 @@ class Video {
         }
     }
 
+    /**
+     * Add a video's JS and CSS components.
+     * This does not create the video's required HTML components.
+     *
+     * @param string $video_id
+     *   The ID for the video.
+     * @param array $settings
+     *   The settings for the video.
+     */
     public static function add($video_id, $settings) {
         self::initDisplay();
         JS::set('videos.' . $video_id, $settings);
