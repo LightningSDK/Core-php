437f67e9772a3a7dc4e4dbcf01232335a53b8e94
DAB <dab@dab.la>
2015-07-16 16:26:09 -0500

    updated pagination to work with base url that already has ?

--- View/Pagination.php
+++ View/Pagination.php
@@ -5,17 +5,18 @@ namespace Lightning\View;
 class Pagination {
     public static function render($base_path, $parameter, $current_page, $total_pages) {
         $output = '';
+        $concatenator = strpos($base_path, '?') !== false ? '&' : '?';
         if ($total_pages > 1) {
             $output .= '<ul class="pagination">';
-            $output .= '<li class="arrow ' . ($current_page > 1 ? '' : 'unavailable') . '"><a href="' . $base_path . '?' . $parameter . '=' . 1 . '">&laquo; First</a></li>';
+            $output .= '<li class="arrow ' . ($current_page > 1 ? '' : 'unavailable') . '"><a href="' . $base_path . $concatenator . $parameter . '=' . 1 . '">&laquo; First</a></li>';
             for($i = max(1, $current_page - 10); $i <= min($total_pages, $current_page + 10); $i++) {
                 if ($current_page == $i) {
                     $output.= '<li class="current">' . $i . '</li>';
                 } else {
-                    $output.= "<li><a href='". $base_path . '?' . $parameter . '=' . $i ."'>{$i}</a></li>";
+                    $output.= "<li><a href='". $base_path . $concatenator . $parameter . '=' . $i ."'>{$i}</a></li>";
                 }
             }
-            $output .= '<li class="arrow ' . ($current_page == $total_pages ? 'unavailable' : '') . '"><a href="' . $base_path . '?' . $parameter . '=' . $total_pages . '">Last &raquo;</a></li>';
+            $output .= '<li class="arrow ' . ($current_page == $total_pages ? 'unavailable' : '') . '"><a href="' . $base_path . $concatenator . $parameter . '=' . $total_pages . '">Last &raquo;</a></li>';
             $output .= '</ul>';
         }
         return $output;
