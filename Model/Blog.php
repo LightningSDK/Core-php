d01adf90819e77ade07544965f2b7c99bfa62d91
DAB <dab@dab.la>
2015-07-16 22:57:01 -0500

    moved blog shorten text to new text view

--- Model/Blog.php
+++ Model/Blog.php
@@ -2,6 +2,7 @@
 
 namespace Overridable\Lightning\Model;
 
+use Lightning\View\Text;
 use Lightning\Tools\Configuration;
 use Lightning\Tools\Database;
 use Lightning\Tools\Scrub;
@@ -50,24 +51,7 @@ class Blog extends Singleton {
     }
 
     function shortBody($body, $length = 250) {
-        $body = str_replace('<', ' <', $body);
-        $body = strip_tags($body);
-        if (strlen($body) <= $length) {
-            return $body;
-        }
-
-        $last_dot = strpos($body, '. ', $length * .8);
-        if ($last_dot >= 1 && $last_dot <= $length * 1.2 ) {
-            //go to the end of the sentence if it's less than 10% longer
-            return substr($body, 0, $last_dot + 1);
-        }
-
-        $last_white = strpos($body, ' ', $length);
-        if ($last_white >= $length) {
-            return substr($body, 0, $last_white) . '...';
-        }
-
-        return $body;
+        Text::shorten($body, $length);
     }
 
     protected function getCategoryID($search_value) {
