d7fcfc7fbf1421bb3ab237aef528d4d9d3b555b4
DAB <dab@dab.la>
2015-12-28 13:02:55 -0600

    updated blog names

--- Database/Schema/BlogCategory.php
+++ Database/Schema/BlogCategory.php
@@ -12,15 +12,15 @@ class BlogCategory extends Schema {
         return array(
             'cat_id' => $this->autoincrement(),
             'category' => $this->varchar(32),
-            'car_url' => $this->varchar(32),
+            'cat_url' => $this->varchar(32),
         );
     }
 
     public function getKeys() {
         return array(
             'primary' => 'cat_id',
-            'car_url' => array(
-                'columns' => array('car_url'),
+            'cat_url' => array(
+                'columns' => array('cat_url'),
                 'unique' => true,
             ),
         );
--- Pages/BlagCategories.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Lightning\Pages;
-
-class BlagCategories extends Table {
-    protected $table = 'blog_category';
-}
