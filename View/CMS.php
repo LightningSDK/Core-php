c6705419cce2a99deb658eeb3359fd85145d57b3
DAB <dab@dab.la>
2016-02-17 18:57:38 -0600

    added error handling for cms

--- View/CMS.php
+++ View/CMS.php
@@ -79,7 +79,9 @@ class CMS {
             . '<input type="text" placeholder="classes" id="cms_' . $name . '_class" class="imagesCSS" name="' . $forced_classes . '" value="' . $added_classes . '" />'
             . '<img src="' . $content->url . '" id="cms_' . $name . '" class="' . $content->class . '" />';
         } else {
-            return '<img src="' . $content->url . '" class="' . $content->class . '" />';
+            if (!empty($content)) {
+                return '<img src="' . $content->url . '" class="' . $content->class . '" />';
+            }
         }
     }
 
