a257180af35d5f9b81bf14fd5fab817eb7260668
DAB <dab@dab.la>
2016-02-20 22:13:52 -0600

    fixed unsubscribe message

--- install/Templates/unsubscribe_confirm.tpl.php
+++ install/Templates/unsubscribe_confirm.tpl.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <h2>Are you sure you want to unsubscribe from all MindPT mailing lists?</h2>
+    <h2>Are you sure you want to unsubscribe from all mailing lists?</h2>
     <form action="/user" method="post">
         <input type="hidden" name="action" value="confirm-unsubscribe" />
         <input type="hidden" name="u" value="<?=\Lightning\Tools\Scrub::toHTML($user_token);?>" />
