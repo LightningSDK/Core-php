ac6f986669ca4da501277a1d0f49701f1be6fe80
DAB <dab@dab.la>
2016-02-26 13:23:01 -0600

    removed extra /a tags from social links

--- View/SocialLinks.php
+++ View/SocialLinks.php
@@ -10,10 +10,10 @@ class SocialLinks {
         JS::startup('lightning.social.initShare()');
         JS::set('social.twitter.url', Configuration::get('social.twitter.url'));
         $output = '<div class="social-share" data-url="' . Scrub::toURL($url) . '">';
-        $output .= '<div class="share facebook"><i class="fa fa-facebook"></i> Share</a></div>';
-        $output .= '<div class="share google"><i class="fa fa-google-plus"></i> Share</a></div>';
-        $output .= '<div class="share twitter"><i class="fa fa-twitter"></i> Tweet</a></div>';
-        $output .= '<div class="share linkedin"><i class="fa fa-linkedin"></i> Linked In</a></div>';
+        $output .= '<div class="share facebook"><i class="fa fa-facebook"></i> Share</div>';
+        $output .= '<div class="share google"><i class="fa fa-google-plus"></i> Share</div>';
+        $output .= '<div class="share twitter"><i class="fa fa-twitter"></i> Tweet</div>';
+        $output .= '<div class="share linkedin"><i class="fa fa-linkedin"></i> Linked In</div>';
         $output .= '<div class="share email"><a href="mailto:?body=' . Scrub::toURL($url) . '"><i class="fa fa-send"></i> Email</a></div>';
         $output .= '</div>';
         return $output;
