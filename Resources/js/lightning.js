93833cfe502616e6976cfe9119c15a6403c62758
DAB <dab@dab.la>
2016-02-29 19:46:24 -0600

    removed form validation

--- Resources/js/lightning.js
+++ Resources/js/lightning.js
@@ -38,39 +38,6 @@ lightning.startup = {
                     return valid;
                 })();
         });
-
-        // Everything below here is deprecated.
-        var forms = $('form.validate');
-
-        if (forms.length == 0) {
-            return;
-        }
-
-        // Add lightning validation rules.
-        $.validator.addMethod("passwordLength", $.validator.methods.minlength, "Your password must be at least {0} characters long.");
-        $.validator.addMethod("passwordVerify", function(value, element, param) {
-            // Copied from $.validator.methods.equalTo
-            var $element = $(element);
-            var target = $element.closest('form').find(param);
-            if ( this.settings.onfocusout ) {
-                target.unbind( ".validate-equalTo" ).bind( "blur.validate-equalTo", function() {
-                    $element.valid();
-                });
-            }
-            return value === target.val();
-        }, "Please enter the same password twice.");
-        $.validator.addClassRules("password", { required: true, passwordLength: 6});
-        $.validator.addClassRules("password2", { required: true, passwordLength: 6, passwordVerify: '.password'});
-
-        // Activate validation for forms.
-        forms.each(function() {
-            var id = $(this).attr('id');
-            if (lightning.formValidation && lightning.formValidation[id]) {
-                $(this).validate(lightning.formValidation[id]);
-            } else {
-                $(this).validate();
-            }
-        });
     }
 };
 lightning.require = function(url, callback) {
