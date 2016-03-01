67cd54192515f17ad537c5125e79d85fca31e4da
DAB <dab@dab.la>
2016-03-01 09:15:14 -0600

    updated installed gruntfile

--- install/Resources/Gruntfile.js
+++ install/Resources/Gruntfile.js
@@ -29,8 +29,31 @@ module.exports = function(grunt) {
                         '../../Lightning/build/js-res/jquery.js',
                         '../../Lightning/build/js-res/placeholder.min.js',
                         '../../Lightning/build/js-res/modernizr.min.js',
+                        // Core - Required
                         '../../Lightning/build/js-res/foundation.js',
-                        '../../Lightning/build/js-res/foundation.*.js',
+                        // Form Validation
+                        '../../Lightning/build/js-res/foundation.abide.js',
+                        //'../../Lightning/build/js-res/foundation.accordion.js',
+                        //'../../Lightning/build/js-res/foundation.alert.js',
+                        // Lightbox / image display:
+                        //'../../Lightning/build/js-res/foundation.clearing.js',
+                        //'../../Lightning/build/js-res/foundation.dropdown.js',
+                        '../../Lightning/build/js-res/foundation.equalizer.js',
+                        // For mobile optimization of images
+                        //'../../Lightning/build/js-res/foundation.interchange.js',
+                        //'../../Lightning/build/js-res/foundation.joyride.js',
+                        //'../../Lightning/build/js-res/foundation.magellan.js',
+                        // Side menu
+                        //'../../Lightning/build/js-res/foundation.offcanvas.js',
+                        //'../../Lightning/build/js-res/foundation.orbit.js',
+                        // Modal
+                        '../../Lightning/build/js-res/foundation.reveal.js',
+                        //'../../Lightning/build/js-res/foundation.slider.js',
+                        // Multi Tab Navigation
+                        //'../../Lightning/build/js-res/foundation.tab.js',
+                        //'../../Lightning/build/js-res/foundation.tooltip.js',
+                        // Nav menu
+                        '../../Lightning/build/js-res/foundation.topbar.js',
                         '../../Lightning/build/js-res/lightning.min.js'
                     ],
                     '../../js/ckeditor/config.js': 'js/ckeditor_config.js',
@@ -42,11 +65,29 @@ module.exports = function(grunt) {
             dist: {
                 files: [
                     {
-                        cwd: '../../Lightning/build',
+                        cwd: '../../Lightning/build/js',
                         src: '**',
-                        dest: '../../',
+                        dest: '../../js/',
                         expand: true
-                    }
+                    },
+                    {
+                        cwd: '../../Lightning/build/css',
+                        src: '**',
+                        dest: '../../css/',
+                        expand: true
+                    },
+                    {
+                        cwd: '../../Lightning/build/fonts',
+                        src: '**',
+                        dest: '../../fonts/',
+                        expand: true
+                    },
+                    {
+                        cwd: '../../Lightning/build/swf',
+                        src: '**',
+                        dest: '../../swf/',
+                        expand: true
+                    },
                 ]
             }
         },
