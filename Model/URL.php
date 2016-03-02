45a7108a7c4868f17e82937173d505d6a240786b
DAB <dab@dab.la>
2016-03-02 12:51:43 -0600

    added url tracking to contact requests

---
+++ Model/URL.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lightning\Model;
+
+use Lightning\Tools\Database;
+use Overridable\Lightning\Tools\Request;
+
+class URL extends Object {
+    const TABLE = 'url';
+    const PRIMARY_KEY = 'url_id';
+    const MAX_LENGTH = 255;
+
+    public static function getCurrentUrlId() {
+        $url = substr(Request::getURL(), 0, static::MAX_LENGTH);
+        $db = Database::getInstance();
+        if (!$id = $db->selectFieldQuery([
+            'select' => static::PRIMARY_KEY,
+            'from' => static::TABLE,
+            'where' => ['url' => ['LIKE', $url]],
+        ], static::PRIMARY_KEY)) {
+            $id = $db->insert(static::TABLE, ['url' => $url]);
+        }
+        return $id;
+    }
+}
