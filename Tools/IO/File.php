7d397d25e4e81f3721205bdf7def3d35025c81b5
DAB <dab@dab.la>
2016-03-26 17:44:25 -0500

    updated file handler and added file hander interface

--- Tools/IO/File.php
+++ Tools/IO/File.php
@@ -2,25 +2,39 @@
 
 namespace Lightning\Tools\IO;
 
-class File {
+class File implements FileHandlerInterface {
+
+    protected $root;
 
     public function __construct($root) {
-        $this->root = $root;
+        if ($root[0] == '/') {
+            $this->root = $root;
+        } else {
+            $this->root = preg_replace('|/+|', '/', HOME_PATH . '/' . $root);
+        }
     }
 
     public function exists($file) {
-        return file_exists(HOME_PATH . '/' . $this->root . '/' . $file);
+        return file_exists($this->root . '/' . $file);
     }
 
     public function read($file) {
-        return file_get_contents(HOME_PATH . '/' . $this->root . '/' . $file);
+        return file_get_contents($this->root . '/' . $file);
     }
 
     public function write($file, $contents) {
-        if (!file_exists(dirname(HOME_PATH . '/' . $this->root . '/' . $file))) {
-            mkdir(dirname(HOME_PATH . '/' . $this->root . '/' . $file), 0777, true);
+        if (!file_exists(dirname($this->root . '/' . $file))) {
+            mkdir(dirname($this->root . '/' . $file), 0777, true);
         }
-        file_put_contents(HOME_PATH . '/' . $this->root . '/' . $file, $contents);
+        file_put_contents($this->root . '/' . $file, $contents);
+    }
+
+    public function moveUploadedFile($file, $temp_file) {
+        move_uploaded_file($temp_file, $this->root . '/' . $file);
+    }
+
+    public function delete($file) {
+        unlink($this->root . '/' . $file);
     }
 
     public function getWebURL($file) {
