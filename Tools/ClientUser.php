fa885a2e8f866dfe73a5fbe6bb961050a4dfdbb2
DAB <dab@dab.la>
2016-05-15 17:18:21 -0500

    added return true to ClientUser::requireLogin()

--- Tools/ClientUser.php
+++ Tools/ClientUser.php
@@ -62,6 +62,9 @@ class ClientUser extends Singleton {
      *
      * @param string $action
      *   The action on the login page.
+     *
+     * @return boolean
+     *   Returns true if they are logged in.
      */
     public static function requireLogin($action = '') {
         if (self::getInstance()->id == 0) {
@@ -79,6 +82,8 @@ class ClientUser extends Singleton {
                 $query['redirect'] .= '?' . http_build_query($redirect_query);
             }
             Navigation::redirect('/user' . $action, $query);
+        } else {
+            return true;
         }
     }
 
