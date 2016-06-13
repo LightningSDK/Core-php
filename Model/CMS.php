cb9585d50174081092acb3299686b8c8cca8e599
DAB <dab@dab.la>
2016-06-13 10:04:25 -0500

    added CMS docblocks

--- Model/CMS.php
+++ Model/CMS.php
@@ -6,11 +6,27 @@ use Lightning\Model\Object;
 use Lightning\Tools\Database;
 use Source\Model\Site;
 
+/**
+ * Class CMS
+ * @package Source\Model
+ *
+ * @parameter integer $id
+ * @parameter integer $cms_id
+ * @parameter string $note
+ * @parameter string $name
+ * @parameter string content
+ * @parameter string class
+ * @parameter integer last_modified
+ */
 class CMS extends Object {
 
     const TABLE = 'cms';
     const PRIMARY_KEY = 'cms_id';
 
+    /**
+     * @param $name
+     * @return CMS
+     */
     public static function loadByName($name) {
         $content = Database::getInstance()->selectRow(static::TABLE, array('name' => $name));
         if ($content) {
