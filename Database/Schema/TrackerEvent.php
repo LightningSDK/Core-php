60ebb748e001d2c7c856492a95badc7c0a2eea42
DAB <dab@dab.la>
2016-09-07 11:20:04 -0500

    updated tracker event indexes

--- Database/Schema/TrackerEvent.php
+++ Database/Schema/TrackerEvent.php
@@ -8,21 +8,21 @@ class TrackerEvent extends Schema {
     const TABLE = 'tracker_event';
 
     public function getColumns() {
-        return array(
+        return [
             'tracker_id' => $this->int(true),
             'user_id' => $this->int(true),
             'session_id' => $this->int(true),
             'sub_id' => $this->int(true),
             'date' => $this->int(true),
             'time' => $this->int(true),
-        );
+        ];
     }
 
     public function getKeys() {
-        return array(
-            'tracker_user' => array(
-                'columns' => array('date', 'tracker_id'),
-            ),
-        );
+        return [
+            'tracker_user' => [
+                'columns' => ['tracker_id', 'user_id'],
+            ],
+        ];
     }
 }
