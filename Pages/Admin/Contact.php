2fbaea38a7c17e9bccc5cc8dfa37123b0dd03b29
DAB <dab@dab.la>
2016-09-29 09:15:02 -0500

    fixed permission error on contact results page

--- Pages/Admin/Contact.php
+++ Pages/Admin/Contact.php
@@ -10,8 +10,6 @@ class Contact extends Table {
     const TABLE = 'user_contact';
     const PRIMARY_KEY = 'contact_id';
 
-    protected $editable = false;
-    protected $viewable = true;
     protected $sort = ['time' => 'DESC'];
     protected $preset = [
         'time' => 'datetime',
