caf5fff24180501e626e6da2fda3c1108a08d6cb
DAB <dab@dab.la>
2016-10-09 12:33:18 -0500

    updated filter query building - required for earlier commits

--- Filter/Filter.php
+++ Filter/Filter.php
@@ -14,7 +14,7 @@ abstract class Filter implements FilterInterface {
         return $this->settings;
     }
 
-    public function filterQuery(&$query, $values) {
+    public function filterQuery($values) {
         // Validate the parameters.
         $validated_values = [];
         foreach ($this->settings['options'] as $name => $options) {
@@ -27,20 +27,29 @@ abstract class Filter implements FilterInterface {
             }
         }
 
+        $query = [];
+
+        $fp = !empty($this->settings['field_table']) ? $this->settings['field_table'] . '.' : '';
+
         // Add the conditions to the query.
         if ($this->settings['type'] == 'operator_value') {
             if ($validated_values['operator'] == '=') {
-                $query['where'][][$this->settings['field']] = $validated_values['value'];
+                $query['where'][][$fp . $this->settings['field']] = $validated_values['value'];
             } else {
-                $query['where'][][$this->settings['field']] = [$validated_values['operator'], $validated_values['value']];
+                $query['where'][][$fp . $this->settings['field']] = [$validated_values['operator'], $validated_values['value']];
             }
         }
 
         // Add the joins.
         if (!empty($this->settings['join'])) {
-            // TODO: Make sure this table is not already joined.
-            $query['join'][] = $this->settings['join'];
+            $query['join'] = $this->settings['join'];
         }
+
+        if (!empty($this->settings['field_table'])) {
+            $query['select'][$this->settings['field_table']] = [$this->settings['field']];
+        }
+
+        return $query;
     }
 }
 
