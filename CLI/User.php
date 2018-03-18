27ece8783449cd6639b1828e08eeeeefc556bad0
DAB <dab@dab.la>
2018-03-18 12:28:59 -0500

    Added explicit password set command

--- CLI/User.php
+++ CLI/User.php
@@ -46,4 +46,24 @@ class User extends CLI {
             $this->out('Failed to create user.');
         }
     }
+
+    public function executeSetPassword() {
+        if (empty($this->parameters['user']) || !Scrub::email($this->parameters['user'])) {
+            throw new \Exception('Invalid email');
+        }
+        $user = UserModel::loadByEmail($this->parameters['user']);
+        if (empty($user)) {
+            throw new \Exception('Invalid user');
+        }
+
+        if (empty($this->parameters['password'])) {
+            do {
+                $password = $this->readline('Password: ', true);
+            } while (strlen($password) < 6);
+        } else {
+            $password = $this->parameters['password'];
+        }
+
+        $user->setPass($password);
+    }
 }
