5268282a1af184fc2eb6034fceab553f225af013
DAB <dab@dab.la>
2018-04-10 23:19:59 -0500

    Added youtube social driver

---
+++ Tools/SocialDrivers/YouTube.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lightning\Tools\SocialDrivers;
+
+use Lightning\Tools\Configuration;
+use Lightning\View\JS;
+
+class YouTube {
+    public static function renderFollow() {
+        if ($youtube_page = Configuration::get('social.youtube.url')) {
+            JS::add('https://apis.google.com/js/platform.js');
+            return '<div class="g-ytsubscribe" data-channelid="' . $youtube_page . '" data-layout="default" data-count="default"></div>';
+        }
+        return '';
+    }
+}
