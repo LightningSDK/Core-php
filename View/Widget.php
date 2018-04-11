32a4a345c8ce23c641eb0e61a5413b786856bb44
DAB <dab@dab.la>
2018-04-11 01:19:14 -0500

    Added 100% width to widgets

--- View/Widget.php
+++ View/Widget.php
@@ -20,10 +20,12 @@ class Widget extends Page {
     public function get() {
         $id = Request::get('id', Request::TYPE_STRING, null, 'lightning_widget');
         $widget = empty($this->widget) ? Request::get('widget') : $this->widget;
+
         $params = $_GET;
         unset($params['request']);
         $params = ['action' => 'body'] + $params;
-        echo 'document.write(\'<iframe frameborder="0" src="' . Configuration::get('web_root') . '/' . $widget . '?' . http_build_query($params) . '" id="' . $id . '"></iframe>\');
+
+        echo 'document.write(\'<iframe frameborder="0" src="' . Configuration::get('web_root') . '/' . $widget . '?' . http_build_query($params) . '" id="' . $id . '" width="100%"></iframe>\');
         if (!lightning || !lightning.widget) {
             document.write(\'<script src="' . Configuration::get('web_root') . '/js/lightning.min.js"></script>\');
         }
