5e296ec61d7e780140644feeb06b2f2e69557a6e
DAB <dab@dab.la>
2018-04-22 08:47:19 -0500

    Added message send to optin api

--- API/Optin.php
+++ API/Optin.php
@@ -4,6 +4,7 @@ namespace Lightning\API;
 
 use Lightning\Model\Message;
 use Lightning\Model\User;
+use Lightning\Tools\Mailer;
 use Lightning\Tools\Messenger;
 use Lightning\Tools\Output;
 use Lightning\Tools\Request;
@@ -14,14 +15,22 @@ class Optin extends API {
         $name = Request::get('name');
         $email = Request::get('email', Request::TYPE_EMAIL);
         $list = Request::get('list', Request::TYPE_INT);
+        $message = Request::get('message', Request::TYPE_INT);
         if (empty($list)) {
             $list = Message::getDefaultListID();
         } else {
             Message::validateListID($list);
         }
 
+        // Subscribe the user
         $user = User::addUser($email, ['full_name' => $name]);
         $user->subscribe($list);
+
+        // Send a message
+        if ($message) {
+            $mailer = new Mailer();
+            $this->userMessageSent = $mailer->sendOne($message, $user);
+        }
 
         Messenger::message('Thank you for subscribing.');
 
