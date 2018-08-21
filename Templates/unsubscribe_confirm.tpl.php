a11825ca68cd3f9dafab4f205507aae1f0cc48f6
DAB <dab@dab.la>
2018-08-21 18:45:20 -0500

    Added token to unsubscribe confirm

--- Templates/unsubscribe_confirm.tpl.php
+++ Templates/unsubscribe_confirm.tpl.php
@@ -1,6 +1,7 @@
 <div class="row">
     <h2>Are you sure you want to unsubscribe from all mailing lists?</h2>
     <form action="/user" method="post">
+        <?= \Lightning\Tools\Form::renderTokenInput(); ?>
         <input type="hidden" name="action" value="confirm-unsubscribe" />
         <input type="hidden" name="u" value="<?=\Lightning\Tools\Scrub::toHTML($user_token);?>" />
         <input type="submit" name="submit" value="Yes" class="button radius" />
