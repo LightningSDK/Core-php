98992509a967e37423b3be52412659703e1d3c73
Dan B <dab@dab.la>
2018-12-23 14:44:49 +0000

    added state options for countries other than the us

--- View/Field/Location.php
+++ View/Field/Location.php
@@ -95,21 +95,53 @@ class Location {
         return $output;
     }
 
-    public static function getStateOptions() {
-        return [
-            'AL' => 'Alabama','AK' => 'Alaska','AZ' => 'Arizona','AR' => 'Arkansas','CA' => 'California',
-            'CO' => 'Colorado','CT' => 'Connecticut','DE' => 'Delaware','DC' => 'District of Columbia',
-            'FL' => 'Florida','GA' => 'Georgia','HI' => 'Hawaii','ID' => 'Idaho','IL' => 'Illinois',
-            'IN' => 'Indiana','IA' => 'Iowa','KS' => 'Kansas','KY' => 'Kentucky','LA' => 'Louisiana',
-            'ME' => 'Maine','MD' => 'Maryland','MA' => 'Massachusetts','MI' => 'Michigan',
-            'MN' => 'Minnesota','MS' => 'Mississippi','MO' => 'Missouri','MT' => 'Montana',
-            'NE' => 'Nebraska','NV' => 'Nevada','NH' => 'New Hampshire','NJ' => 'New Jersey',
-            'NM' => 'New Mexico','NY' => 'New York','NC' => 'North Carolina','ND' => 'North Dakota',
-            'OH' => 'Ohio','OK' => 'Oklahoma','OR' => 'Oregon','PA' => 'Pennsylvania',
-            'RI' => 'Rhode Island','SC' => 'South Carolina','SD' => 'South Dakota',
-            'TN' => 'Tennessee','TX' => 'Texas','UT' => 'Utah','VT' => 'Vermont','VA' => 'Virginia',
-            'WA' => 'Washington','WV' => 'West Virginia','WI' => 'Wisconsin','WY' => 'Wyoming'
+    public static function getStateOptions($country = 'US') {
+        static $states = [
+            'US' => [
+                'AL' => 'Alabama','AK' => 'Alaska','AZ' => 'Arizona','AR' => 'Arkansas','CA' => 'California',
+                'CO' => 'Colorado','CT' => 'Connecticut','DE' => 'Delaware','DC' => 'District of Columbia',
+                'FL' => 'Florida','GA' => 'Georgia','HI' => 'Hawaii','ID' => 'Idaho','IL' => 'Illinois',
+                'IN' => 'Indiana','IA' => 'Iowa','KS' => 'Kansas','KY' => 'Kentucky','LA' => 'Louisiana',
+                'ME' => 'Maine','MD' => 'Maryland','MA' => 'Massachusetts','MI' => 'Michigan',
+                'MN' => 'Minnesota','MS' => 'Mississippi','MO' => 'Missouri','MT' => 'Montana',
+                'NE' => 'Nebraska','NV' => 'Nevada','NH' => 'New Hampshire','NJ' => 'New Jersey',
+                'NM' => 'New Mexico','NY' => 'New York','NC' => 'North Carolina','ND' => 'North Dakota',
+                'OH' => 'Ohio','OK' => 'Oklahoma','OR' => 'Oregon','PA' => 'Pennsylvania',
+                'RI' => 'Rhode Island','SC' => 'South Carolina','SD' => 'South Dakota',
+                'TN' => 'Tennessee','TX' => 'Texas','UT' => 'Utah','VT' => 'Vermont','VA' => 'Virginia',
+                'WA' => 'Washington','WV' => 'West Virginia','WI' => 'Wisconsin','WY' => 'Wyoming'
+            ],
+            'CA' => [
+                'AB' => 'Alberta', 'BC' => 'British Columbia', 'MB' => 'Manitoba', 'NB' => 'New Brunswick',
+                'NL' => 'Newfoundland and Labrador', 'NS' => 'Nova Scotia', 'NT' => 'Northwest Territories',
+                'NU' => 'Nunavut', 'ON' => 'Ontario', 'PE' => 'Prince Edward Island', 'QC' => 'Quebec',
+                'SK' => 'Saskatchewan', 'YT' => 'Yukon'
+            ],
+            'MX' => [
+                'AG' => 'Aguascalientes', 'BC' => 'Baja California', 'BS' => 'Baja California Sur',
+                'CM' => 'Campeche', 'CS' => 'Chiapas', 'CH' => 'Chihuahua', 'CO' => 'Coahuila',
+                'CL' => 'Colima', 'DF' => 'Mexico City', 'DG' => 'Durango', 'GT' => 'Guanajuato',
+                'GR' => 'Guerrero', 'HG' => 'Hidalgo', 'JA' => 'Jalisco', 'EM' => 'México',
+                'MI' => 'Michoacán', 'MO' => 'Morelos', 'NA' => 'Nayarit', 'NL' => 'Nuevo León',
+                'OA' => 'Oaxaca', 'PU' => 'Puebla', 'QT' => 'Querétaro', 'QR' => 'Quintana Roo',
+                'SL' => 'San Luis Potosí', 'SI' => 'Sinaloa', 'SO' => 'Sonora', 'TB' => 'Tabasco',
+                'TM' => 'Tamaulipas', 'TL' => 'Tlaxcala', 'VE' => 'Veracruz', 'YU' => 'Yucatán',
+                'ZA' => 'Zacatecas',
+            ],
+            'AU' => [
+                'NSW' => 'New South Wales', 'QLD' => 'Queensland', 'SA' => 'South Australia',
+                'TAS' => 'Tasmania', 'VIC' => 'Victoria', 'WA' => 'Western Australia',
+                'ACT' => 'Australian Capital Territory', 'NT' => 'Northern Territory',
+            ]
         ];
+
+        if ($country === null) {
+            return $states;
+        } elseif (isset($states[$country])) {
+            return $states[$country];
+        } else {
+            return null;
+        }
     }
 
     /**
