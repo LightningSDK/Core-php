e0fedf28475a81f9b260813d6d202b4412e27808
DAB <dab@dab.la>
2020-06-19 00:42:42 -0400

    fixed startup issues, added 'document' dependency, encapsulated lightning js components

--- js/components/fileBrowser.js
+++ js/components/fileBrowser.js
@@ -1,126 +1,128 @@
-lightning.fileBrowser = {
-    type: null,
-    field: null,
-    elf: null,
-    funcNum: null,
-    init: function() {
-        var query = document.location.search.substring(1).split('&');
-        for (var i in query) {
-            var split = query[i].split('=');
-            if (split[0] == 'type') {
-                lightning.fileBrowser.type = split[1];
+(function(){
+    var self = lightning.fileBrowser = {
+        type: null,
+        field: null,
+        elf: null,
+        funcNum: null,
+        init: function() {
+            var query = document.location.search.substring(1).split('&');
+            for (var i in query) {
+                var split = query[i].split('=');
+                if (split[0] === 'type') {
+                    self.type = split[1];
+                }
+                if (split[0] === 'field') {
+                    self.field = split[1];
+                }
+                if (split[0] === 'CKEditorFuncNum') {
+                    self.funcNum = split[1];
+                }
+                if (split[0] === 'url') {
+                    self.urlType = split[1];
+                }
+                if (split[0] === 'web_root') {
+                    self.webRoot = unescape(split[1]);
+                }
             }
-            if (split[0] == 'field') {
-                lightning.fileBrowser.field = split[1];
+
+            // TODO: This has to be changed to handle other browsers.
+            self.elf = $('#elfinder').elfinder({
+                // set your elFinder options here
+                url: '/api/elfinder',  // connector URL
+                closeOnEditorCallback: true,
+                getFileCallback: self.select,
+                height: '100%',
+                resizable: false
+            }).elfinder('instance');
+        },
+
+        select: function(file, urlType) {
+            var url = file.url;
+            if (self.urlType == 'full') {
+                url = (self.webRoot + '/' + url).replace(/([^:])\/\/+/g, '$1/');
             }
-            if (split[0] == 'CKEditorFuncNum') {
-                lightning.fileBrowser.funcNum = split[1];
+            switch (self.type) {
+                case 'ckeditor':
+                    window.opener.CKEDITOR.tools.callFunction(self.funcNum, url);
+                    window.close();
+                    break;
+                case 'tinymce':
+                    // pass selected file path to TinyMCE
+                    parent.tinymce.activeEditor.windowManager.getParams().setUrl(url);
+
+                    // force the TinyMCE dialog to refresh and fill in the image dimensions
+                    var t = parent.tinymce.activeEditor.windowManager.windows[0];
+                    t.find('#src').fire('change');
+
+                    // close popup window
+                    parent.tinymce.activeEditor.windowManager.close();
+                    break;
+                case 'lightning-field':
+                    window.opener.self.fieldSelected(url, self.field);
+                    window.close();
+                    break;
+                case 'lightning-cms':
+                    window.opener.lightning.cms.imageSelected(url, self.field);
+                    window.close();
+                    break;
             }
-            if (split[0] == 'url') {
-                lightning.fileBrowser.urlType = split[1];
+        },
+
+        /**
+         * Open the image browser window for selection.
+         *
+         * @param {string} type
+         *   The browser type to use: elfinder, ckfinder, etc.
+         * @param {string} field
+         *   The type the type of the parent: ckeditor, tinymce, lightning-field, lightning-cms
+         * @param {string} url
+         *   The url type to use: full, absolute
+         */
+        openSelect: function(type, field, url) {
+            if (typeof url == 'undefined') {
+                url = 'absolute&web_root=' + escape(lightning.vars.web_root);
             }
-            if (split[0] == 'web_root') {
-                lightning.fileBrowser.webRoot = unescape(split[1]);
+
+            // TODO: add case here for other browser types.
+            switch (self.type || lightning.vars.fileBrowser.type) {
+                case 'elfinder':
+                    window.open(
+                        '/elfinder?type=' + type + '&field=' + field + '&url=' + url,
+                        'Image Browser',
+                        ''
+                    );
+                    break;
+                case 'ckfinder':
+                    CKFinder.popup({
+                        basePath: lightning.vars.cms.basepath,
+                        chooseFiles: true,
+                        chooseFilesOnDblClick: true,
+                        onInit: function( finder ) {
+                            finder.on( 'files:choose', function( evt ) {
+                                var file = evt.data.files.first();
+                                self.updateImage(id, file.getUrl());
+                            } );
+                            finder.on( 'file:choose:resizedImage', function( evt ) {
+                                self.updateImage(id, evt.data.resizedUrl);
+                            } );
+                        }
+                    });
+                    break;
             }
+        },
+        fieldSelected: function(url, field) {
+            var imageField = $('#file_browser_image_' + field);
+            if (imageField.length > 0) {
+                imageField.prop('src', url).show();
+            }
+            $('#' + field).val(url);
+        },
+        clear: function(field) {
+            var imageField = $('#file_browser_image_' + field);
+            if (imageField.length > 0) {
+                imageField.prop('src', '').hide();
+            }
+            $('#' + field).val('');
         }
-
-        // TODO: This has to be changed to handle other browsers.
-        lightning.fileBrowser.elf = $('#elfinder').elfinder({
-            // set your elFinder options here
-            url: '/api/elfinder',  // connector URL
-            closeOnEditorCallback: true,
-            getFileCallback: lightning.fileBrowser.select,
-            height: '100%',
-            resizable: false
-        }).elfinder('instance');
-    },
-
-    select: function(file, urlType) {
-        var url = file.url;
-        if (lightning.fileBrowser.urlType == 'full') {
-            url = (lightning.fileBrowser.webRoot + '/' + url).replace(/([^:])\/\/+/g, '$1/');
-        }
-        switch (lightning.fileBrowser.type) {
-            case 'ckeditor':
-                window.opener.CKEDITOR.tools.callFunction(lightning.fileBrowser.funcNum, url);
-                window.close();
-                break;
-            case 'tinymce':
-                // pass selected file path to TinyMCE
-                parent.tinymce.activeEditor.windowManager.getParams().setUrl(url);
-
-                // force the TinyMCE dialog to refresh and fill in the image dimensions
-                var t = parent.tinymce.activeEditor.windowManager.windows[0];
-                t.find('#src').fire('change');
-
-                // close popup window
-                parent.tinymce.activeEditor.windowManager.close();
-                break;
-            case 'lightning-field':
-                window.opener.lightning.fileBrowser.fieldSelected(url, lightning.fileBrowser.field);
-                window.close();
-                break;
-            case 'lightning-cms':
-                window.opener.lightning.cms.imageSelected(url, lightning.fileBrowser.field);
-                window.close();
-                break;
-        }
-    },
-
-    /**
-     * Open the image browser window for selection.
-     *
-     * @param {string} type
-     *   The browser type to use: elfinder, ckfinder, etc.
-     * @param {string} field
-     *   The type the type of the parent: ckeditor, tinymce, lightning-field, lightning-cms
-     * @param {string} url
-     *   The url type to use: full, absolute
-     */
-    openSelect: function(type, field, url) {
-        if (typeof url == 'undefined') {
-            url = 'absolute&web_root=' + escape(lightning.vars.web_root);
-        }
-
-        // TODO: add case here for other browser types.
-        switch (lightning.fileBrowser.type || lightning.vars.fileBrowser.type) {
-            case 'elfinder':
-                window.open(
-                    '/elfinder?type=' + type + '&field=' + field + '&url=' + url,
-                    'Image Browser',
-                    ''
-                );
-                break;
-            case 'ckfinder':
-                CKFinder.popup({
-                    basePath: lightning.vars.cms.basepath,
-                    chooseFiles: true,
-                    chooseFilesOnDblClick: true,
-                    onInit: function( finder ) {
-                        finder.on( 'files:choose', function( evt ) {
-                            var file = evt.data.files.first();
-                            self.updateImage(id, file.getUrl());
-                        } );
-                        finder.on( 'file:choose:resizedImage', function( evt ) {
-                            self.updateImage(id, evt.data.resizedUrl);
-                        } );
-                    }
-                });
-                break;
-        }
-    },
-    fieldSelected: function(url, field) {
-        var imageField = $('#file_browser_image_' + field);
-        if (imageField.length > 0) {
-            imageField.prop('src', url).show();
-        }
-        $('#' + field).val(url);
-    },
-    clear: function(field) {
-        var imageField = $('#file_browser_image_' + field);
-        if (imageField.length > 0) {
-            imageField.prop('src', '').hide();
-        }
-        $('#' + field).val('');
-    }
-};
+    };
+})();
---
+++ js/components/js.js
@@ -0,0 +1,108 @@
+(function(){
+    var self = lightning.js = {
+        queue: [],
+        loaded: {},
+        loadScript: function(scriptURL) {
+            self.loaded[scriptURL] = false;
+            var script = document.createElement('script');
+            script.src = scriptURL + '?v=' + lightning.get('minified_version');
+            script.type = 'text/javascript';
+            script.async = 'true';
+            script.onload = script.onreadystatechange = function() {
+                var rs = this.readyState;
+                if (rs && rs != 'complete' && rs != 'loaded') return;
+                self.loaded[scriptURL] = true;
+                self.trigger();
+            };
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(script, s);
+        },
+
+        /**
+         * Execute a callback as soon as all required JS scripts are loaded.
+         *
+         * @param {array|string} urls
+         * @param {function} callback
+         */
+        require: function(urls, callback) {
+            if (typeof urls === "string") {
+                urls = [urls];
+            }
+
+            self.queue.push({
+                urls: urls,
+                callback: callback,
+                triggered: false
+            });
+
+            for (var i in urls) {
+                if (!self.loaded.hasOwnProperty(urls[i])) {
+                    // Any scripts that are not already included can be included here.
+                    if (urls[i].match(/\.js\?v=[0-9]+/)) {
+                        self.loadScript(urls[i]);
+                    }
+                }
+            }
+
+            self.trigger();
+        },
+
+        /**
+         * Attempts to run any callbacks whose dependencies have loaded
+         */
+        trigger: function() {
+            // Iterate over each queued item.
+            queue:
+                for (var i in self.queue) {
+                    // See if all scripts are loaded.
+                    if (!self.queue[i].triggered) {
+                        for (var j in self.queue[i].urls) {
+                            // Trigger the script.
+                            var url = self.queue[i].urls[j];
+                            if (
+                                // this checkso for js files and "document"
+                                !self.loaded.hasOwnProperty(url)
+                                // this works for 'lightning' or '$' jquery
+                                && ("undefined" === typeof window[url] || "document" === url)
+                            ) {
+                                continue queue;
+                            }
+                        }
+                        self.queue[i].triggered = true;
+                        self.queue[i].callback();
+                    }
+                }
+        },
+
+        documentOnReady: function() {
+            self.loaded['document'] = true;
+            self.trigger();
+        },
+
+        /**
+         * Any scripts that were requested to run before the require function was established
+         * will be added to the require queue.
+         */
+        loadStartupQueue: function() {
+            // When this happens lightnign.js is loaded
+            self.loaded['/js/lightning.min.js'] = true;
+
+            // Set up notification for when the document is ready
+            window.onload = self.documentOnReady;
+            if (document.readyState === 'complete') {
+                self.documentOnReady();
+            }
+
+            for (var i in $lsq) {
+                self.require($lsq[i].r, $lsq[i].c);
+            }
+        },
+
+        addModule: function(name, module) {
+            if("undefined" == typeof lightning.modules) {
+                lightning.modules = {};
+            }
+            lightning.modules[name] = module;
+        }
+    };
+})();
