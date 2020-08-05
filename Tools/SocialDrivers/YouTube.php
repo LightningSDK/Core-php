9dad29fe6cf904bfc750208a7421933316ed1190
DAB <dab@dab.la>
2020-08-05 15:32:42 -0400

    fix google follow

--- Tools/SocialDrivers/YouTube.php
+++ Tools/SocialDrivers/YouTube.php
@@ -8,8 +8,7 @@ use lightningsdk\core\View\JS;
 class YouTube {
     public static function renderFollow() {
         if ($youtube_page = Configuration::get('social.youtube.url')) {
-            JS::add('https://apis.google.com/js/platform.js');
-            return '<div class="g-ytsubscribe" data-channel="' . $youtube_page . '" data-layout="default" data-count="default"></div>';
+            return '<script src="https://apis.google.com/js/platform.js"></script><div class="g-ytsubscribe" data-channel="' . $youtube_page . '" data-layout="default" data-count="default"></div>';
         }
         return '';
     }
